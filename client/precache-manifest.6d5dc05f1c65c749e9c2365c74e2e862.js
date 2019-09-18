4f24c5cfdcdd0499d3e3c57b813767aba5871443
Phuong Tran <ptran816@uw.edu>
2019-09-18 02:24:14 -0700

    Updates

---
+++ client/precache-manifest.6d5dc05f1c65c749e9c2365c74e2e862.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06ef23cf26306a59fff1ad9c4c48e46e",
+    "url": "/drawing/index.html"
+  },
+  {
+    "revision": "272094139cb69d5f605f",
+    "url": "/drawing/static/css/main.36af5723.chunk.css"
+  },
+  {
+    "revision": "d0f525607af3114d6d37",
+    "url": "/drawing/static/js/2.789494f8.chunk.js"
+  },
+  {
+    "revision": "272094139cb69d5f605f",
+    "url": "/drawing/static/js/main.2756167f.chunk.js"
+  },
+  {
+    "revision": "75d348bfbad82d475a14",
+    "url": "/drawing/static/js/runtime~main.85a24b30.js"
+  }
+]);
