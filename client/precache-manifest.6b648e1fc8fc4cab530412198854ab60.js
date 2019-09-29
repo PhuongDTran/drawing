6cd45e3a7418d5529a56c4c57758300f11e686b9
Phuong Tran <ptran816@uw.edu>
2019-09-29 14:27:57 -0700

    Updates

---
+++ client/precache-manifest.6b648e1fc8fc4cab530412198854ab60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d87531853a78fd081a770aff1ea0f90b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0459cf741076f0f6ec52",
+    "url": "/static/css/main.36af5723.chunk.css"
+  },
+  {
+    "revision": "91ca6d72b75c4ea0f771",
+    "url": "/static/js/2.fe6a8218.chunk.js"
+  },
+  {
+    "revision": "0459cf741076f0f6ec52",
+    "url": "/static/js/main.63f0a344.chunk.js"
+  },
+  {
+    "revision": "4cfe237cf4a00a29d17d",
+    "url": "/static/js/runtime~main.79010ad5.js"
+  }
+]);
